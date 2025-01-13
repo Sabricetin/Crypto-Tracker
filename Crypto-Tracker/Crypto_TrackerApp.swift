2bd0d54b93c0c71638f90410240c828993924ec5
Sabricetin <sabriyazilim@gmail.com>
2025-01-13 11:41:19 +0300

    Initial Commit

---
+++ Crypto-Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto-Tracker
+//
+//  Created by Sabri Çetin on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
