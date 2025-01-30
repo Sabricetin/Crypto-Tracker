94dff6410d8c69f2e7c61ab5bc5f00b3856a4865
Sabricetin <sabriyazilim@gmail.com>
2025-01-30 11:55:55 +0300

    Launch Screen
    Added Start Screen and Start Animation

--- Crypto-Tracker/Crypto_TrackerApp.swift
+++ Crypto-Tracker/Crypto_TrackerApp.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct Crypto_TrackerApp: App {
     
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView : Bool = true
+    
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
@@ -19,11 +21,26 @@ struct Crypto_TrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                HomeView()
-                    .navigationBarHidden(true)
+            
+            ZStack {
+                
+                NavigationView {
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+
+                    }
+                }
+                .zIndex(2.0)
+                
             }
-            .environmentObject(vm)
+           
         }
     }
 }
